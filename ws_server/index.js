72cba79bebae78e2981c3506ef23a9cd7a763ab3
Robin Markowitz <loepoe71@gmail.com>
2018-10-03 17:33:58 -0700

    Implement simple server

--- ws_server/index.js
+++ ws_server/index.js
@@ -1,11 +1,13 @@
 const WebSocket = require('ws');
+const BuoyHelpers = require('./buoy_helpers.js');
 
-const server = new Websocket.Server({
+const server = new WebSocket.Server({
   port: 8080,
 });
 
 server.on('connection', (ws) => {
   ws.on('message', (data) => {
-    // handle rcp request
+    const request = JSON.parse(data);
+    BuoyHelpers[request.method](request.params);
   });
 });
