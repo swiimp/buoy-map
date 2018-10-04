645c429a6b15dddc58c91314bac309d96d9b711a
Robin Markowitz <loepoe71@gmail.com>
2018-10-03 17:33:09 -0700

    Add skeleton helper functions

---
+++ ws_server/buoy_helpers.js
@@ -0,0 +1,25 @@
+const buoys = {};
+const clients = {};
+
+const addBuoy = (params) => {
+
+};
+
+const updateBuoyData = (params) => {
+
+};
+
+const subscribeToBuoys = (params) => {
+
+};
+
+const buoyNotification = (params) => {
+
+};
+
+module.exports = {
+  addBuoy: addBuoy,
+  updateBuoyData: updateBuoyData,
+  subscribeToBuoys: subscribeToBuoys,
+  buoyNotification: buoyNotification,
+};
