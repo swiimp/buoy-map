c6d958fdb0aef89ec3c789f7438826075d3b70fe
Robin Markowitz <loepoe71@gmail.com>
2018-10-06 17:01:21 -0700

    Refactor for bugfix

--- ws_server/buoy_helpers.js
+++ ws_server/buoy_helpers.js
@@ -10,7 +10,7 @@ const addBuoy = (params, cb) => {
       lon: params.lon,
       height: null,
       period: null,
-      clients: [],
+      clients: {},
     };
     _latIndex.splice(_findIndex(params.lat, 'lat'), 0, {
       name: params.name,
@@ -27,8 +27,8 @@ const addBuoy = (params, cb) => {
         params.lat < _clients[client].bounds.north &&
         params.lon > _clients[client].bounds.west &&
         params.lon < _clients[client].bounds.east) {
-          _buoys[params.name].clients.push(client);
-          _clients[client].buoys.push(params.name);
+          _buoys[params.name].clients[client] = client;
+          _clients[client].buoys[params.name] = params.name;
           // Add client to cb payload
       }
     }
@@ -45,33 +45,30 @@ const updateBuoyData = (params, cb) => {
   cb();
 };
 
-const subscribeToBuoys = (params, cb, clientId) => {
+const subscribeToBuoys = (params, cb, client) => {
   if (_clients[clientId] === undefined ||
       _clients[clientId].bounds.west !== params.west ||
       _clients[clientId].bounds.east !== params.east ||
       _clients[clientId].bounds.south !== params.south ||
       _clients[clientId].bounds.north !== params.north) {
-    const results = [];
+    const results = {};
 
     if (_latIndex.length > 0) {
       const isWithinBounds = (buoy) => buoy.lat > params.south && buoy.lat < params.north &&
                                         buoy.lon > params.west && buoy.lon < params.east;
-      const addedBuoys = {};
       let iLat = _findIndex(params.south, 'lat');
       let iLon = _findIndex(params.west, 'lon');
       while (iLat < _latIndex.length && iLon < _lonIndex.length &&
               _latIndex[iLat].lat < params.north && _lonIndex[iLon].lon < params.east) {
-        if (addedBuoys[_latIndex[iLat].name] !== true && isWithinBounds(_latIndex[iLat])) {
-          results.push(_latIndex[iLat].name);
-          _buoys[_latIndex[iLat].name].clients.push(clientId);
+        if (results[_latIndex[iLat].name] === undefined && isWithinBounds(_latIndex[iLat])) {
+          results[_latIndex[iLat].name] = _latIndex[iLat].name;
+          _buoys[_latIndex[iLat].name].clients[client] = client;
           // Add buoy to cb payload
-          addedBuoys[_latIndex[iLat].name] = true;
         }
-        if (addedBuoys[_lonIndex[iLon].name] !== true && isWithinBounds(_lonIndex[iLon])) {
-          results.push(_lonIndex[iLon].name);
-          _buoys[_lonIndex[iLon].name].clients.push(clientId);
+        if (results[_lonIndex[iLon].name] === undefined && isWithinBounds(_lonIndex[iLon])) {
+          results[_lonIndex[iLon].name] = _lonIndex[iLon].name;
+          _buoys[_lonIndex[iLon].name].clients[client] = client;
           // Add buoy to cb payload
-          addedBuoys[_lonIndex[iLon].name] = true;
         }
         iLat += 1;
         iLon += 1;
