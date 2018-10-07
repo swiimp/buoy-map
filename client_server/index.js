f84bc91a2be2af3edda7a4133059edbbd2d02fe9
Robin Markowitz <loepoe71@gmail.com>
2018-10-07 11:41:43 -0700

    Add ws skeleton

--- client_server/index.js
+++ client_server/index.js
@@ -1,17 +1,29 @@
 const path = require('path');
 const express = require('express');
+const WebSocket = require('ws');
 
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.APP_PORT || 3000;
+const wsHost = process.env.WS_HOST || `ws://localhost:8080`;
 
 app.use(express.static(path.join(__dirname, './public')));
 
 io.on('connection', (socket) => {
   console.log(`io: Connection on ${socket.handshake.headers.referer}
                 from ${socket.handshake.address}`);
+  const ws = new WebSocket(wsHost);
+  ws.on('message', (data) => {
+    const response = JSON.parse(data);
+    if (response.id /* && there's an error */) {
+      // Handle error
+    } else {
+      // Handle notification
+    }
+  });
 });
+
 
 http.listen(port, () => {
   console.log(`App listening on port ${port}...`);
