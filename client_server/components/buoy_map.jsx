338b9754bb7fe6168af29188a30aa5123039eb3b
Robin Markowitz <loepoe71@gmail.com>
2018-10-10 21:26:52 -0700

    Remove old box controls

--- client_server/components/buoy_map.jsx
+++ client_server/components/buoy_map.jsx
@@ -9,13 +9,12 @@ class BuoyMap extends React.Component {
     super(props);
     this.state = {
       buoys: {},
-      west: '',
-      south: '',
-      east: '',
-      north: '',
+      west: null,
+      south: null,
+      east: null,
+      north: null,
     };
 
-    this.handleChange = this.handleChange.bind(this);
     this.subscribeToBuoys = this.subscribeToBuoys.bind(this);
     this.buoyNotification = this.buoyNotification.bind(this);
     this.setBounds = this.setBounds.bind(this);
@@ -25,19 +24,6 @@ class BuoyMap extends React.Component {
       const req = JSON.parse(data);
       this.buoyNotification(req.params);
     });
-  }
-
-  handleChange(e) {
-    const newState = {};
-    newState[e.target.id] = e.target.value;
-    this.setState(newState);
-  }
-
-  handleEnter(e) {
-    e.preventDefault();
-    if (e.key === 'Enter') {
-      this.subscribeToBuoys();
-    }
   }
 
   subscribeToBuoys() {
@@ -82,49 +68,6 @@ class BuoyMap extends React.Component {
           buoys={this.state.buoys}
           setBounds={this.setBounds}
         />
-        <form className="map-controls">
-          <div className="map-controls-col">
-            <label for="west">West</label>
-            <input
-            id="west"
-            className="latlon-input"
-            type="text"
-            value={this.state.west}
-            onChange={this.handleChange}
-            onKeyUp={this.handleEnter}
-            />
-            <label for="east">East</label>
-            <input
-            id="east"
-            className="latlon-input"
-            type="text"
-            value={this.state.east}
-            onChange={this.handleChange}
-            onKeyUp={this.handleEnter}
-            />
-          </div>
-          <div className="map-controls-col">
-            <label for="south">South</label>
-            <input
-            id="south"
-            className="latlon-input"
-            type="text"
-            value={this.state.south}
-            onChange={this.handleChange}
-            onKeyUp={this.handleEnter}
-            />
-            <label for="north">North</label>
-            <input
-            id="north"
-            className="latlon-input"
-            type="text"
-            value={this.state.north}
-            onChange={this.handleChange}
-            onKeyUp={this.handleEnter}
-            />
-          </div>
-        </form>
-        <button className="bound-subscribe" onClick={this.subscribeToBuoys}>Submit</button>
       </div>
     );
   }
--- client_server/components/map.jsx
+++ client_server/components/map.jsx
@@ -4,9 +4,6 @@ import {defaultStyle} from '../utils/default_style.js'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import BuoyOverlay from './buoy_overlay.jsx'
-import CanvasOverlay from 'react-map-gl/src/overlays/canvas-overlay.js'
-
-let animation = null;
 
 class Map extends React.Component {
   constructor(props) {
@@ -18,67 +15,20 @@ class Map extends React.Component {
       latitude: 34.454,
       longitude: -120.783,
       zoom: 8,
-      isDrawing: false,
       mousePos: null,
-      startCorner: null,
-      endCorner: null,
     };
 
-    this.handleClick = this.handleClick.bind(this);
     this.handleHover = this.handleHover.bind(this);
-    this.drawBox = this.drawBox.bind(this);
     this.addOverlay = this.addOverlay.bind(this);
   }
 
-  handleClick(e) {
-    if (this.state.isDrawing) {
-      this.setState({ isDrawing: false });
-      window.cancelAnimationFrame(animation);
-      this.props.setBounds(this.state.startCorner, this.state.endCorner);
-    } else {
-      this.setState({
-        isDrawing: true,
-        startCorner: e.lngLat,
-      });
-    }
-  }
-
   handleHover(e) {
-    if (this.state.isDrawing) {
-      animation = window.requestAnimationFrame(() =>
-        this.setState({
-          mousePos: e.lngLat,
-          endCorner: e.lngLat,
-        }));
-    } else {
-      this.setState({ mousePos: e.lngLat });
-    }
-  }
-
-  drawBox(params) {
-    const rectStart = params.project(this.state.startCorner);
-    const rectEnd = params.project(this.state.endCorner);
-    const rectWidth = rectEnd[0] - rectStart[0];
-    const rectHeight = rectEnd[1] - rectStart[1];
-
-    params.ctx.clearRect(0, 0, params.width, params.height);
-    params.ctx.globalCompositeOperation = 'source-over';
-    params.ctx.fillStyle = 'rgba(0, 255, 0, 0.25)';
-    params.ctx.strokeStyle = 'rgb(0, 255, 0)';
-    params.ctx.fillRect(rectStart[0], rectStart[1], rectWidth, rectHeight);
+    this.setState({ mousePos: e.lngLat });
   }
 
   addOverlay(buoys, startCorner, endCorner) {
     const childNodes = [];
 
-    if (endCorner && startCorner) {
-      childNodes.push(
-        <CanvasOverlay
-          id='box'
-          redraw={this.drawBox}
-        />
-      );
-    }
     if (Object.keys(buoys).length > 0) {
       childNodes.push(
         <BuoyOverlay
@@ -115,10 +65,9 @@ class Map extends React.Component {
             zoom: viewport.zoom,
           })
         }
-        onClick={this.handleClick}
         onHover={this.handleHover}
         mapboxApiAccessToken={MapboxAccessToken}>
-        {this.addOverlay(this.props.buoys, this.state.startCorner, this.state.endCorner)}
+        {this.addOverlay(this.props.buoys)}
       </ReactMapGL>
     );
   }
