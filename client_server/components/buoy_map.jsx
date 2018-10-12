a6474a138a0cff69abb013343fa4c850c0e20748
Robin Markowitz <loepoe71@gmail.com>
2018-10-12 11:54:19 -0700

    Add BuoyMap docs

--- client_server/components/buoy_map.jsx
+++ client_server/components/buoy_map.jsx
@@ -4,6 +4,13 @@ import io from 'socket.io-client';
 import Map from './map.jsx';
 import '../utils/main.css';
 
+// name: BuoyMap
+// description: Top-level component for the BuoyMap application. Manages socket.io communication
+//              with client server.
+//
+// props:
+//   'lonPrecache' -- Float; the longitude offset for fetching buoys outside the viewport
+//   'latPrecache' -- Float; the latitude offset for fetching buoys outside the viewport
 class BuoyMap extends React.Component {
   constructor(props) {
     super(props);
@@ -19,6 +26,7 @@ class BuoyMap extends React.Component {
     this.buoyNotification = this.buoyNotification.bind(this);
     this.setBounds = this.setBounds.bind(this);
 
+    // get an instance of a socket.io client
     this.socket = io();
     this.socket.on('notification', (data) => {
       const req = JSON.parse(data);
@@ -26,6 +34,13 @@ class BuoyMap extends React.Component {
     });
   }
 
+  // name: subscribeToBuoys
+  // description: Sends a json-rcp 2.0 request for the 'subscribeToBuoys' function.
+  //
+  // params:
+  //   none
+  //
+  // returns: nothing
   subscribeToBuoys() {
     const rpc = {
       jsonrpc: '2.0',
@@ -40,6 +55,13 @@ class BuoyMap extends React.Component {
     this.socket.send(JSON.stringify(rpc));
   }
 
+  // name: buoyNotification
+  // description: Updates the specified buoy state with the given information.
+  //
+  // params:
+  //   'buoy' -- Object; the updated version of the given buoy
+  //
+  // returns: nothing
   buoyNotification(buoy) {
     const newBuoys = Object.assign({}, this.state.buoys);
     newBuoys[buoy.name] = {
@@ -52,12 +74,21 @@ class BuoyMap extends React.Component {
     this.setState({ buoys: newBuoys });
   }
 
+  // name: setBounds
+  // description: Updates the state of the bounds if the given bounds fall outside the current ones.
+  //
+  // params:
+  //   'bounds' -- LngLatBounds; a MapboxGL LngLatBounds object containing the bounds of the
+  //               current viewport
+  //
+  // returns: nothing
   setBounds(bounds) {
     if (bounds.getWest() < this.state.west ||
         bounds.getSouth() < this.state.south ||
         bounds.getEast() > this.state.east ||
         bounds.getNorth() > this.state.north) {
       this.setState({
+        // apply precache offsets to retrieve off-screen buoys
         west: bounds.getWest() - this.props.lonPrecache,
         south: bounds.getSouth() - this.props.latPrecache,
         east: bounds.getEast() + this.props.lonPrecache,
