30a85b03cc29d00a0b57d0760509745213a0d8c0
Robin Markowitz <loepoe71@gmail.com>
2018-10-11 20:44:56 -0700

    Refactor tests for ES6

--- ws_server/__tests__/buoy_helpers.test.js
+++ ws_server/__tests__/buoy_helpers.test.js
@@ -1,21 +1,9 @@
-const BuoyHelpers = require('../buoy_helpers.js');
+import BuoyHelpers from '../buoy_helpers.js';
 
-describe('BuoyHelpers', () => {
-  afterEach(() => {
-    for (let buoy in BuoyHelpers._buoys) {
-      delete BuoyHelpers._buoys[buoy];
-    }
-    for (let client in BuoyHelpers._clients) {
-      delete BuoyHelpers._clients[client];
-    }
-    while (BuoyHelpers._latIndex.length > 0) {
-      BuoyHelpers._latIndex.pop();
-      BuoyHelpers._lonIndex.pop();
-    }
-  });
-
+describe('buoyHelpers', () => {
   describe('addBuoy', () => {
     test('should add a new entry for new buoys', () => {
+      const buoyHelpers = new BuoyHelpers();
       const params = {
         name: 'jest_buoy_0',
         lat: 42,
@@ -31,12 +19,13 @@ describe('BuoyHelpers', () => {
         },
       };
 
-      BuoyHelpers.addBuoy(params, () => {
-        expect(BuoyHelpers._buoys).toEqual(expectedBuoys);
+      buoyHelpers.addBuoy(params, () => {
+        expect(buoyHelpers._buoys).toEqual(expectedBuoys);
       });
     });
 
     test('should not overwrite old buoys', () => {
+      const buoyHelpers = new BuoyHelpers();
       const params_1 = {
         name: 'jest_buoy_0',
         lat: 42,
@@ -57,14 +46,15 @@ describe('BuoyHelpers', () => {
         },
       };
 
-      BuoyHelpers.addBuoy(params_1, () => {
-        BuoyHelpers.addBuoy(params_2, () => {
-          expect(BuoyHelpers._buoys).toEqual(expectedBuoys);
+      buoyHelpers.addBuoy(params_1, () => {
+        buoyHelpers.addBuoy(params_2, () => {
+          expect(buoyHelpers._buoys).toEqual(expectedBuoys);
         });
       });
     });
 
     test('should add the buoy to any appropriate subscriptions', () => {
+      const buoyHelpers = new BuoyHelpers();
       const params_1 = {
         name: 'jest_buoy_0',
         lat: 0,
@@ -103,17 +93,18 @@ describe('BuoyHelpers', () => {
         },
       };
 
-      BuoyHelpers.subscribeToBuoys({ west: -43, east: -41, south: 41, north: 43 }, () => {
-        BuoyHelpers.addBuoy(params_1, () => {
-          BuoyHelpers.addBuoy(params_2, () => {
-            expect(BuoyHelpers._clients).toEqual(expectedClients);
-            expect(BuoyHelpers._buoys).toEqual(expectedBuoys);
+      buoyHelpers.subscribeToBuoys({ west: -43, east: -41, south: 41, north: 43 }, () => {
+        buoyHelpers.addBuoy(params_1, () => {
+          buoyHelpers.addBuoy(params_2, () => {
+            expect(buoyHelpers._clients).toEqual(expectedClients);
+            expect(buoyHelpers._buoys).toEqual(expectedBuoys);
           });
         });
       }, 'client_0');
     });
 
     test('should properly index buoys', () => {
+      const buoyHelpers = new BuoyHelpers();
       const params_1 = {
         name: 'jest_buoy_0',
         lat: 0,
@@ -147,12 +138,12 @@ describe('BuoyHelpers', () => {
         { name: 'jest_buoy_3', lat: -10, lon: 100 },
       ];
 
-      BuoyHelpers.addBuoy(params_1, () => {
-        BuoyHelpers.addBuoy(params_2, () => {
-          BuoyHelpers.addBuoy(params_3, () => {
-            BuoyHelpers.addBuoy(params_4, () => {
-              expect(BuoyHelpers._latIndex).toEqual(expectedLatIndex);
-              expect(BuoyHelpers._lonIndex).toEqual(expectedLonIndex);
+      buoyHelpers.addBuoy(params_1, () => {
+        buoyHelpers.addBuoy(params_2, () => {
+          buoyHelpers.addBuoy(params_3, () => {
+            buoyHelpers.addBuoy(params_4, () => {
+              expect(buoyHelpers._latIndex).toEqual(expectedLatIndex);
+              expect(buoyHelpers._lonIndex).toEqual(expectedLonIndex);
             });
           });
         });
@@ -162,6 +153,7 @@ describe('BuoyHelpers', () => {
 
   describe('updateBuoyData', () => {
     test('should update the target buoy with the given data', () => {
+      const buoyHelpers = new BuoyHelpers();
       const params_1 = {
         name: 'jest_buoy_0',
         lat: 0,
@@ -182,47 +174,38 @@ describe('BuoyHelpers', () => {
         },
       };
 
-      BuoyHelpers.addBuoy(params_1, () => {
-        BuoyHelpers.updateBuoyData(params_2, () => {
-          expect(BuoyHelpers._buoys).toEqual(expectedBuoys);
+      buoyHelpers.addBuoy(params_1, () => {
+        buoyHelpers.updateBuoyData(params_2, () => {
+          expect(buoyHelpers._buoys).toEqual(expectedBuoys);
         });
       });
     });
   });
 
   describe('subscribeToBuoys', () => {
-    beforeEach(() => {
-      const test_param_1 = {
-        name: 'jest_buoy_0',
-        lat: 0,
-        lon: 0,
-      };
-      const test_param_2 = {
-        name: 'jest_buoy_1',
-        lat: 42,
-        lon: -42,
-      };
-      const test_param_3 = {
-        name: 'jest_buoy_2',
-        lat : 30,
-        lon : -30,
-      };
-      const test_param_4 = {
-        name: 'jest_buoy_3',
-        lat: -10,
-        lon: 100,
-      };
-
-      BuoyHelpers.addBuoy(test_param_1, () => {
-        BuoyHelpers.addBuoy(test_param_2, () => {
-          BuoyHelpers.addBuoy(test_param_3, () => {
-            BuoyHelpers.addBuoy(test_param_4, () => 0);
-          });
-        });
-      });
-    });
+    const test_param_1 = {
+      name: 'jest_buoy_0',
+      lat: 0,
+      lon: 0,
+    };
+    const test_param_2 = {
+      name: 'jest_buoy_1',
+      lat: 42,
+      lon: -42,
+    };
+    const test_param_3 = {
+      name: 'jest_buoy_2',
+      lat : 30,
+      lon : -30,
+    };
+    const test_param_4 = {
+      name: 'jest_buoy_3',
+      lat: -10,
+      lon: 100,
+    };
 
     test('should add a new entry for new subscriptions', () => {
+      const buoyHelpers = new BuoyHelpers();
       const params = {
         south: 29,
         west: -43,
@@ -244,12 +227,21 @@ describe('BuoyHelpers', () => {
         },
       };
 
-      BuoyHelpers.subscribeToBuoys(params, () => {
-        expect(BuoyHelpers._clients).toEqual(expectedClients);
-      }, 'client_0');
+      buoyHelpers.addBuoy(test_param_1, () => {
+        buoyHelpers.addBuoy(test_param_2, () => {
+          buoyHelpers.addBuoy(test_param_3, () => {
+            buoyHelpers.addBuoy(test_param_4, () => {
+              buoyHelpers.subscribeToBuoys(params, () => {
+                expect(buoyHelpers._clients).toEqual(expectedClients);
+              }, 'client_0');
+            });
+          });
+        });
+      });
     });
 
     test('should add the subscriptions to any appropriate buoys', () => {
+      const buoyHelpers = new BuoyHelpers();
       const params = {
         south: 29,
         west: -43,
@@ -287,12 +279,21 @@ describe('BuoyHelpers', () => {
         },
       };
 
-      BuoyHelpers.subscribeToBuoys(params, () => {
-        expect(BuoyHelpers._buoys).toEqual(expectedBuoys);
-      }, 'client_0');
+      buoyHelpers.addBuoy(test_param_1, () => {
+        buoyHelpers.addBuoy(test_param_2, () => {
+          buoyHelpers.addBuoy(test_param_3, () => {
+            buoyHelpers.addBuoy(test_param_4, () => {
+              buoyHelpers.subscribeToBuoys(params, () => {
+                expect(buoyHelpers._buoys).toEqual(expectedBuoys);
+              }, 'client_0');
+            });
+          });
+        });
+      });
     });
 
     test('should overwrite old subscriptions', () => {
+      const buoyHelpers = new BuoyHelpers();
       const params_1 = {
         south: -11,
         west: -43,
@@ -350,12 +351,20 @@ describe('BuoyHelpers', () => {
         },
       };
 
-      BuoyHelpers.subscribeToBuoys(params_1, () => {
-        BuoyHelpers.subscribeToBuoys(params_2, () => {
-          expect(BuoyHelpers._clients).toEqual(expectedClients);
-          expect(BuoyHelpers._buoys).toEqual(expectedBuoys);
-        }, 'client_0');
-      }, 'client_0');
+      buoyHelpers.addBuoy(test_param_1, () => {
+        buoyHelpers.addBuoy(test_param_2, () => {
+          buoyHelpers.addBuoy(test_param_3, () => {
+            buoyHelpers.addBuoy(test_param_4, () => {
+              buoyHelpers.subscribeToBuoys(params_1, () => {
+                buoyHelpers.subscribeToBuoys(params_2, () => {
+                  expect(buoyHelpers._clients).toEqual(expectedClients);
+                  expect(buoyHelpers._buoys).toEqual(expectedBuoys);
+                }, 'client_0');
+              }, 'client_0');
+            });
+          });
+        });
+      });
     });
   });
 });
